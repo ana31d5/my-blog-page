f63083f7da3fc3ac513e0b447c1f729e03bae09c
Anastasia Enyioma <ana.enyioma@hotmail.com>
2022-04-20 11:52:05 +0100

    Header and nav update

--- my-blog/src/components/Header/header.jsx
+++ my-blog/src/components/Header/header.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/ME.jpg'
+import HeaderSocials from './HeaderSocials'
 
 const header = () => {
   return (
-    <div>header</div>
+
+    <header>
+      <div className='container header_container'>
+        <h5> Hello I'm </h5>
+        <h1> Anastasia Enyioma </h1>
+        <h5 className='text-light'> Fullstack Developer </h5>
+
+        <CTA/>
+        <HeaderSocials/>
+        
+
+        <div className='me'>
+          <img src= {ME} alt='./assets/ME.jpg' />
+        </div>
+            
+        <a href='#contact' className='scroll_down'> Scroll Down </a>
+        
+
+      </div>
+
+    </header>
+   
   )
 }
 
--- my-blog/src/components/Nav/nav.jsx
+++ my-blog/src/components/Nav/nav.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import './nav.css'
+import {AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineUser} from 'react-icons/ai'
+import {BiBook} from 'react-icons/bi'
+import {RiServiceLine} from 'react-icons/ri'
+import {BiMessageSquareDetail} from 'react-icons/bi'
 
 const nav = () => {
   return (
-    <div>nav</div>
+    <nav>
+
+      <a href='#'><AiOutlineHome/></a>
+      <a href='#About'><AiOutlineUser/></a>
+      <a href='#Experience'><BiBook/></a>
+      <a href='#Services'><RiServiceLine/></a>
+      <a href='#Contact'><BiMessageSquareDetail/></a>
+
+    </nav>
   )
 }
 
