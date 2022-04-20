0ba90528fc15f0ba6c9348999bac5df6a1394fc3
Anastasia Enyioma <ana.enyioma@hotmail.com>
2022-04-20 18:52:54 +0100

    experience added

--- my-blog/src/components/Nav/Nav.jsx
+++ my-blog/src/components/Nav/Nav.jsx
@@ -20,7 +20,7 @@ const Nav = () => {
       <a href='#contact'onClick={() => setActiveNav ('#contact')} className={activeNav == '#contact' ? 'active ' : '' }><BiMessageSquareDetail/></a>
 
     </nav>
-  )
+  ) 
 }
 
 export default Nav
